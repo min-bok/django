fac901470ee3052b60b56f75298840c7fec9e721
John Doe <leehy0782@naver.com>
2023-04-12 16:30:15 +0900

    템플릿 사용, 템플릿 상속, 템플릿 확장

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(req):
-    return HttpResponse("here is our products!")
+    return render(req, "projects/projects.html")
 
 def project(req, pk):
-    return HttpResponse("single project" + pk)
+    return render(req, "projects/single-project.html")
