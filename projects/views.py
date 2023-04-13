abe6d5ca36a5bb33aa7ed3e07877f125dd9fa5fb
John Doe <leehy0782@naver.com>
2023-04-13 18:00:46 +0900

    save

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(req):
-    return render(req, "projects/projects.html")
+    page = "projects"
+    number = 9
+    context = {'page': page, "number":number}
+    return render(req, "projects/projects.html", context)
 
 def project(req, pk):
     return render(req, "projects/single-project.html")
