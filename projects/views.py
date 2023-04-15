2ee387aff8293fa61319af183dfac810f893726a
min-bok <leehy0782@gmail.com>
2023-04-15 14:10:18 +0900

    model form

--- projects/views.py
+++ projects/views.py
@@ -1,24 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
-
-projectsList = [
-    {
-     'id' : '1',
-     'title': '쇼핑몰', 
-     'description': '상품판매를 위한 웹사이트입니다.'
-    },
-    {
-     'id' : '2',
-     'title': '포트폴리오', 
-     'description': '포트폴리오 웹사이트입니다.'
-    },
-    {
-     'id' : '3',
-     'title': '학습사이트', 
-     'description': '학습을 위한 웹사이트입니다.'
-    },
-]
+from .forms import ProjectForm
 
 def projects(req):
     projects = Project.objects.all()
@@ -28,3 +11,8 @@ def projects(req):
 def project(req, pk):
     projectObj = Project.objects.get(id=pk)
     return render(req, "projects/single-project.html", {'project':projectObj})
+
+def createProject(req):
+    form = ProjectForm()
+    context = {'form':form}
+    return render(req, "projects/project_form.html", context)
