8b6dd0c4d4a8db1009b7a411bd613eb1a94181f0
min-bok <leehy0782@gmail.com>
2023-04-15 15:36:52 +0900

    handle static files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Django
+
+https://www.udemy.com/course/python-django-2021-complete-course/learn/lecture/27270328#content
+
+15분 46초 부터
--- pythonproject/settings.py
+++ pythonproject/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
